78614c002b1cd1d13c8f28ead17366f53be9450e
Pratheek P Shettigar <pratheek070707@gmail.com>
2026-04-09 12:22:09 +0530

    Create  packet_logger.py

---
+++ packet_logger.py
@@ -0,0 +1,42 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet import ethernet, arp, icmp, tcp, udp
+
+log = core.getLogger()
+
+def _handle_PacketIn(event):
+    packet = event.parsed
+
+    if not packet.parsed:
+        return
+
+    log.info("Packet: %s -> %s", packet.src, packet.dst)
+
+    if packet.type == ethernet.ARP_TYPE:
+        log.info("Protocol: ARP")
+
+    elif packet.type == ethernet.IP_TYPE:
+        ip = packet.payload
+        log.info("IP: %s -> %s", ip.srcip, ip.dstip)
+
+        # ✅ CORRECT (NO .icmp)
+        if isinstance(ip.payload, icmp):
+            log.info("Protocol: ICMP")
+
+        elif isinstance(ip.payload, tcp):
+            log.info("Protocol: TCP")
+
+        elif isinstance(ip.payload, udp):
+            log.info("Protocol: UDP")
+
+    # ✅ Forward packet
+    msg = of.ofp_packet_out()
+    msg.data = event.ofp
+    msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+    msg.in_port = event.port
+    event.connection.send(msg)
+
+
+def launch():
+    log.info("Packet Logger Running 🚀")
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
